629bbfa38bc0bcca1e10c02a8304866f009f851b
Denis Espinoza <denisjev@gmail.com>
2023-07-04 22:22:13 -0600

    Second commit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { DocentesModule } from './docentes/docentes.module';
 
 @Module({
-  imports: [],
+  imports: [DocentesModule],
   controllers: [AppController],
   providers: [AppService],
 })
